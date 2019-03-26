be34e0a277d93add9d26548dd95b18832dab5179
Katherine Wren <kwren@outlook.com>
2019-03-26 18:05:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,8 @@
 var playlist = {artistname:'song'}
 function updatePlaylist(playlist, artistName, songTitle) { playlist['artistName'] = 'songTitle'; return playlist}
 function removeFromPlaylist(playlist,artistName) {delete playlist.artistname; return playlist}
+
+
+//accepts three parameters - playlist an object;artist name a string; and a song title.
+//body of function adds song and artist as key value pair to playlist object.
+//function returns whole playlist
