6cf7766c1921a97095e78477d431ea4195d24dab
Katherine Wren <kwren@outlook.com>
2019-03-26 17:39:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,4 +2,4 @@ var meals = {breakfast: "oatmeal"};
 var meals = new Object({breakfast:'oatmeal'});
 var playlist = {artistName:'song'}
 function updatePlaylist(playlist, artistName, songTitle) { 'playlist'['artistName'] = 'songTitle'}
-function removeFromPlaylist(playlist,artistName){return[delete playlist.artistname]}
+function removeFromPlaylist(playlist,artistName){return delete playlist[artistname]}
