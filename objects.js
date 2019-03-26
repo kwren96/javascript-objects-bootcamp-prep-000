e1acbcb63ba3bf86ee01f1045bccc9b575ff150d
Katherine Wren <kwren@outlook.com>
2019-03-26 17:49:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,2 +1,3 @@
 var playlist = {artistName:'song'}
+function updatePlaylist(playlist, artistName, songTitle) { playlist['artistName'] = 'songTitle'}
 function removeFromPlaylist(playlist,artistName) {delete playlist.artistname; return playlist}
